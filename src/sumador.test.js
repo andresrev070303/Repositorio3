069a58b393abf12b5e480a740aabb33478574b4f
Andres Revollo <andres.revollo@usb.edu.bo>
2024-02-13 20:02:20 -0400

    commit multiplicador

--- src/sumador.test.js
+++ src/sumador.test.js
@@ -1,7 +1,14 @@
 import sumar from "./sumador.js";
+import multiplicar from "./multiplicador.js";
 
 describe("Sumar", () => {
-  it("deberia sumar dos numeros", () => {
+  it("debería sumar dos números", () => {
     expect(sumar(3, 2)).toEqual(5);
+  });
+});
+
+describe("Multiplicar", () => {
+  it("debería multiplicar dos números", () => {
+    expect(multiplicar(3, 2)).toEqual(6);
   });
 });
